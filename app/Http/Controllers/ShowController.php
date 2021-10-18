e86f3712b1b255e40c5db216dd328fc35fabb3fe
shuinoue-s <80799067+shuinoue-s@users.noreply.github.com>
2021-10-18 14:56:37 +0900

    Update ShowController.php

--- app/Http/Controllers/ShowController.php
+++ app/Http/Controllers/ShowController.php
@@ -8,12 +8,28 @@ use App\Models\Comment;
 use App\Models\Tag;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class ShowController extends Controller
 {
+    public function GetPresignedURL(string $s3_key)
+    {
+        $s3 = Storage::disk('s3');
+        $client = $s3->getDriver()->getAdapter()->getClient();
+        $command = $client->getCommand('GetObject', [
+            'Bucket' => env('AWS_BUCKET'),
+            'Key' => $s3_key
+        ]);
+        $request = $client->createPresignedRequest($command, "+10 minutes");
+        return (string) $request->getUri();
+    }
+
     public function index()
     {
         $posts = Post::all()->sortByDesc('created_at');
+        foreach ($posts as $post) {
+            $post['file_path'] = $this->GetPresignedURL($post['file_path']);
+        }
 
         return view('index', compact('posts'));
     }
@@ -21,6 +37,7 @@ class ShowController extends Controller
     public function show($id)
     {
         $post = Post::findOrFail($id);
+        $post['file_path'] = $this->GetPresignedURL($post['file_path']);
         $date = $post->created_at->format('Y/m/d');
         $auth = Auth::id();
         $comments = Comment::where('post_id', $id)->latest()->get();
