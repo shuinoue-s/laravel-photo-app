26e5dc6555f959563a1b688c398938895f66ef42
shuinoue-s <i.shushu0803@gmail.com>
2021-10-19 14:13:07 +0900

    Update: S3に保存した画像を表示するよう変更

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -8,9 +8,24 @@ use App\Http\Requests\TagRequest;
 
 class TagController extends Controller
 {
+    public function GetPresignedURL(string $s3_key)
+    {
+        $s3 = Storage::disk('s3');
+        $client = $s3->getDriver()->getAdapter()->getClient();
+        $command = $client->getCommand('GetObject', [
+            'Bucket' => env('AWS_BUCKET'),
+            'Key' => $s3_key
+        ]);
+        $request = $client->createPresignedRequest($command, "+10 minutes");
+        return (string) $request->getUri();
+    }
+    
     public function list($id) {
         $tag = Tag::findOrFail($id);
         $posts = $tag->posts;
+        foreach ($posts as $post) {
+            $post['file_path'] = $this->GetPresignedURL($post['file_path']);
+        }
 
         return view('tag.list', compact('tag', 'posts'));
     }
@@ -29,6 +44,9 @@ class TagController extends Controller
         if ($tags->isNotEmpty()) {
             foreach ($tags as $tag) {
                 $posts = $tag->posts;
+                foreach ($posts as $post) {
+                    $post['file_path'] = $this->GetPresignedURL($post['file_path']);
+                }
 
                 return view('tag.result', compact('posts', 'tag_name'));
             }
